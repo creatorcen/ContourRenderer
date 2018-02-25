1b7104e203b6e54c3b038b889e673a4c3ec43969
Paul Nigsch <paul.nigsch@ctbto.org>
2018-02-25 15:06:53 +0100

    inital commit

---
+++ parse_contour.py
@@ -0,0 +1,112 @@
+
+import xml.etree.cElementTree  as ET
+import logging
+
+import json
+
+
+import argparse
+
+
+filename_osm= ""
+
+def parseNode(ele):
+    node_id = ele.get("id")
+
+    node_data = {}
+    node_data["lat"]  = ele.get("lat")
+    node_data["lon"]  = ele.get("lon")
+
+
+    return node_id, node_data
+
+
+def parseWay(ele, nodes):
+
+
+    way_id = ele[1].get("id")
+    way_nodes = []
+    way_tags = {} 
+
+
+    for child in ele[1]:
+        if child.tag == 'nd':
+            node_id = child.get("ref")
+
+            way_nodes.append( nodes[node_id])
+
+        elif child.tag == 'tag':
+            k = child.get('k')
+            v = child.get('k')
+
+            way_tags[k] = v
+
+        else:
+            asdf
+
+    return way_id, { 'tags': way_tags, 'nodes' : way_nodes}
+
+
+def main():
+
+
+    nodes = dict()
+
+
+
+    count = 0
+
+
+    # first run: parse nodes
+    xml_it = ET.iterparse(filename_osm,  events=('start', 'end'))
+    for child in xml_it:
+
+        if child[0] == 'start' and child[1].tag == 'node' :
+
+            ele = child[1]
+
+            node_id, node_data = parseNode(ele)
+            nodes[node_id] = node_data
+
+
+
+
+
+
+    # 2nd run: parse ways
+    xml_it = ET.iterparse(filename_osm,  events=('start', 'end'))
+
+    ways = dict()
+    count =0 
+    for child in xml_it:
+
+        if child[0] == 'start' and child[1].tag == 'way' :
+
+
+            way_id, way_data = parseWay(child, nodes)
+            ways[way_id] = way_data
+
+
+
+
+        count+=1
+        if count % 100 == 0:
+            logging.info('parsing node %i', count)
+
+
+
+    with open(filename_osm + '.json', 'w') as f:
+        json.dump(ways, f)
+
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('file',  help='file')
+
+
+args = parser.parse_args()
+print( args.file)
+
+filename_osm = args.file
+
+main()
+
---
+++ phyghtmap_example.sh
@@ -0,0 +1,1 @@
+phyghtmap -a 9.4524:46.8283:10.2585:47.6043 --source=view3 -s 50  5N47E009.hgt N47E010.hgt
